fc5f3d1c91b6862777c1673e89ee689772a0bfa2
twothreetwentythreenft <123201423+twothreetwentythreenft@users.noreply.github.com>
2023-01-20 19:38:04 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+						   
+	$(".countdown").countdown({
+				date: "3 feb 2023 12:00:01",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
